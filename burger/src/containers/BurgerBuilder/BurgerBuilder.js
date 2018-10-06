14ba0609dae7258bfe356e91e1ee1c3684e2e092
Gaurav Gupta <gouravgupta840@gmail.com>
2018-10-06 22:49:39 +0530

    Made http POST request in burgerBuilder.js with axios

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from './../../components/Burger/Burger';
 import BuildControls from './../../components/Burger/BuildControls/BuildControls';
 import Modal from './../../components/UI/Modal/Modal';
 import OrderSummary from './../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 
 const INGREDIENT_PRICES = {
@@ -52,7 +53,26 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () =>{
-        alert('You Continue!');
+        // alert('You Continue!');
+        const order = {
+            ingredients:this.state.ingredients,
+            price:this.state.totalPrice,
+            customer:{
+                name:'GauravGupta',
+                address:{
+                    street:"JSR",
+                    zipCode:'4132256',
+                    country:"norway"
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod:'fastest'
+        }
+
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+
     }
 
     addIngredientHandler = (type) => {
